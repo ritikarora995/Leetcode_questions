2c80007220feccb1e3cab4e132606bbf460a906c
RITIK ARORA <ritikarora995@gmail.com>
2022-06-29 03:47:09 +0530

    Time: 117 ms (92.94%), Space: 67.8 MB (53.28%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums);
-         for(int i = 1; i < size(nums); i++) 
-            dp[i] = max(nums[i], nums[i] + dp[i-1]);
-        
-         return *max_element(begin(dp), end(dp));
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
     }
 };
