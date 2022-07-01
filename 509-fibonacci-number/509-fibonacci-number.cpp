172405083a39f3b57b8cc5005b178469dd98a7ed
RITIK ARORA <ritikarora995@gmail.com>
2022-07-02 00:53:34 +0530

    Time: 4 ms (52.92%), Space: 5.9 MB (41.38%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int fib1[31];
-    // vector<int>v;
+  
     int fib(int n) {
-        if(n<=1){
-           fib1[n]=n;
-            return fib1[n];
+        int dp[31];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        fib1[n]=fib(n-1)+fib(n-2);
-        return fib1[n];
+        return dp[n];
     }
 };
