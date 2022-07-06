302e31e0b61906cd9c955d7246ffece516a40fba
RITIK ARORA <ritikarora995@gmail.com>
2022-07-06 22:46:46 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int abc(int m,int n,vector<vector<int>>&dp){
-        // cout<<m<<n<<c<<endl;
-        if(m==1 && n==1){
-           
-            return 1;
-        }
-        if(m<1 || n<1){
-            return 0;
-        }
-        
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        
-        
-        dp[m][n]=abc(m,n-1,dp)+ abc(m-1,n,dp);
-       return dp[m][n];
-    }
     int uniquePaths(int m, int n) {
-    
-     
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-      return abc(m,n,dp);
-        
-      
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+            return dp[m-1][n-1];
         
     }
 };
