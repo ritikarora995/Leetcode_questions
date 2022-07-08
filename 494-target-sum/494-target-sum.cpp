68cafb8ab00206c722bfee0eeceebc870e7cce96
RITIK ARORA <ritikarora995@gmail.com>
2022-07-08 11:57:08 +0530

    Time: 50 ms (42.71%), Space: 39.8 MB (18.71%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -7,16 +7,16 @@ public:
             return 1;
         else if(begin==nums.size())
             return 0;
-        if(dp[begin][sum+total]!=-1)
-            return dp[begin][sum+total];
+        if(dp[begin][sum+1001]!=-1)
+            return dp[begin][sum+1001];
         int result = solve(nums,sum-nums[begin],target,begin+1,dp) +
             solve(nums,sum+nums[begin],target,begin+1,dp);
-        return dp[begin][sum+total]=result;
+        return dp[begin][sum+1001]=result;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n =  nums.size();
         for(auto num:nums) total+=num;
-        vector<vector<int>> dp(n+1,vector<int>(2*total+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(2*1000+2,-1));
         return solve(nums,0,target,0,dp);
     }
 
