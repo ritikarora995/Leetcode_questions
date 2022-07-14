f714dc3860040a1ed1a77f6c97442aaca5e21785
RITIK ARORA <ritikarora995@gmail.com>
2022-07-15 00:21:23 +0530

    Time: 20 ms (64.47%), Space: 13.1 MB (81.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& a) {
-      set<int>si;
-        set<int>sy;
-        for(int i=0;i<a.size();i++){
-             for(int j=0;j<a[0].size();j++){
-                 if(a[i][j]==0){
-                      si.insert(i);
-                      sy.insert(j);
-                 }
-             }}
-        //  for (auto it = si.begin(); it != si.end(); it++)
-        // cout << *it << " ";
+        int R=a.size();
+        int C=a[0].size();
+        bool iscol=false;
         
-        for(int i=0;i<a.size();i++){
-             for(int j=0;j<a[0].size();j++){
-                if(si.count(i)>0 || sy.count(j)>0){
-                    a[i][j]=0;
+        for(int i=0;i<R;i++){
+            if(a[i][0]==0){
+                iscol=true;
+            }
+            for(int j=1;j<C;j++){
+                if(a[i][j]==0){
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
-             }}
-        
+            }}
+           for(int i=1;i<R;i++){
+               for(int j=1;j<C;j++){
+                   if(a[i][0]==0 || a[0][j]==0){
+                       a[i][j]=0;
+                   }
+               }}
+            if(a[0][0]==0){
+                for(int j=0;j<C;j++){
+                    a[0][j]=0;
+                }
+            }
+            if(iscol){
+                for(int i=0;i<R;i++){
+                    a[i][0]=0;
+                }
+            }
     }
 };
