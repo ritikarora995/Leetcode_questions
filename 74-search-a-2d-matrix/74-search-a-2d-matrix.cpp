00897b4bb7c0dbaffb1cbf84b69777c4b53b4085
RITIK ARORA <ritikarora995@gmail.com>
2022-07-15 15:17:31 +0530

    Time: 4 ms (77.53%), Space: 9.4 MB (78.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool binaryseatch(int row,int start,int end,vector<vector<int>>& a,int target){
+       
+        while(start<=end){
+        int mid=(start+end)/2;
+            if(a[row][mid]==target) return true;
+            else if(a[row][mid]<target) start=mid+1;
+            else if(a[row][mid]>target) end=mid-1;
+        }
+        return false;
         
-        for(int i=0;i<matrix.size();i++){
-             for(int j=0;j<matrix[0].size();j++){
-             if(target==matrix[i][j]){
-                 return true;
-             }
-             }
-             }
-        return 0;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // 1 3 5  7
+            // bool flag=false;
+        int m=matrix[0].size();
+        int n=matrix.size();
+            for(int i=0;i<n;i++){
+                if(binaryseatch(i,0,m-1,matrix,target)) return true;
+            }
+return false;
     }
 };
