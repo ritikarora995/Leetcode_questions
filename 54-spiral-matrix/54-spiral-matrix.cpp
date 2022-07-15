29ec4adcda090aaf3d068de5d0dcc8af56b359e6
RITIK ARORA <ritikarora995@gmail.com>
2022-07-15 14:38:22 +0530

    Time: 6 ms (11.81%), Space: 7 MB (30.15%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -6,24 +6,40 @@ public:
         int m=a[0].size();
         int drn=1;
         int top=0,bottom=n-1,left=0,right=m-1;
-        while(left<=right && top<=bottom){
+        
+        int c=0;
+        while(c!=n*m){
+            
             if(drn==1){
-                for(int i=left;i<=right;i++) v.push_back(a[top][i]);
+                for(int i=left;i<=right;i++) {
+                    v.push_back(a[top][i]);
+                    c++;
+                }
                 top++;
+                
                 drn=2;
             }
             else if(drn==2){
-                for(int i=top;i<=bottom;i++) v.push_back(a[i][right]);
+                for(int i=top;i<=bottom;i++){
+                    
+                 v.push_back(a[i][right]);
+                    c++;}
                 right--;
                 drn=3; 
             }
             else if(drn==3){
-                for(int i=right;i>=left;i--) v.push_back(a[bottom][i]);
+                for(int i=right;i>=left;i--) {
+                    v.push_back(a[bottom][i]);
+                    c++;
+                }
                 bottom--;
                 drn=4;
             }
             else if(drn==4){
-                 for(int i=bottom;i>=top;i--) v.push_back(a[i][left]);
+                 for(int i=bottom;i>=top;i--) {
+                     v.push_back(a[i][left]);
+                     c++;
+                 }
                 left++;
                 drn=1;
             }
