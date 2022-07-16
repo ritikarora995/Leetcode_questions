9773eeaaf102105a5d0c6c0485748d7a9b5eeb5a
RITIK ARORA <ritikarora995@gmail.com>
2022-07-16 11:08:53 +0530

    Time: 9 ms (22.99%), Space: 5.9 MB (24.89%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,23 @@
-class Solution {
+class Solution
+{
 public:
     int mySqrt(int x) {
-                return sqrt(x);
-
+    int low=0;
+     int high=x;
+        int ans;
+        while(low<=high){
+           long long int mid=(low+high)/2;
+            // cout<<mid<<endl;
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x){
+                 high=mid-1;
+             }   
+            else if(mid*mid<x){
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+        
     }
 };
