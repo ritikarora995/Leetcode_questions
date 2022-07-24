950f95cf2426afbd74b3e76f488b9ac62e10b321
RITIK ARORA <ritikarora995@gmail.com>
2022-07-25 02:41:33 +0530

    Time: 2 ms (42.57%), Space: 5.8 MB (87.16%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.cpp
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -1,59 +1,24 @@
-class Solution 
-{
+class Solution {
 public:
-    int maximumSwap(int num) 
-    {
-        vector<int>nums;
-        
-        
-        int c=-1,ans=0,k=0,d=0,e=0;
-        
-        while(num>0)
-        {
-            int c=num%10;
-            num=num/10;
-            nums.push_back(c);
+    int maximumSwap(int num) {
+        string numString=to_string(num);
+        int n=numString.length();
+        vector<int>dpPosition(n,-1);
+        int curMaxPos = n - 1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (numString[i] > numString[curMaxPos]) {
+            curMaxPos = i;
         }
-        vector<int>vk(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.end());
-        sort(vk.begin(),vk.end(),greater<int>());
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(vk[i]!=nums[i])
-            {
-                c=i;
-                d=vk[i];
-                break;
-            }
-        }
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==d)
-                e=i;
-        }
-        
-        if(c==-1)
-        {
-            while(k<nums.size())
-            {
-                ans+=nums[k];
-                ans*=10;
-                k++;
-            }
-        }
-        else
-        {
-            swap(nums[c],nums[e]);
-             while(k<nums.size())
-             {
-                    ans+=nums[k];
-                    ans*=10;
-                    k++;
-             }   
-        }
-        
-        return ans/10;
+        dpPosition[i] = curMaxPos;
     }
+        
+    for (int i = 0; i < n; i++) {
+        if(numString[dpPosition[i]] != numString[i]) {
+            swap(numString[i], numString[dpPosition[i]]);
+            break;
+        }
+    }
+        
+    return stoi(numString);
+}
 };
