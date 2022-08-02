df876eae5d0fac9c31ef75abca320d018198febe
RITIK ARORA <ritikarora995@gmail.com>
2022-08-02 18:20:19 +0530

    Time: 4 ms (79.32%), Space: 9.3 MB (31.17%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string s;
+        for(int i=0;i<strs[0].length();i++){
+            if(strs[0][i]==strs[strs.size()-1][i]){
+               s+=strs[0][i];
+            }
+            else 
+            {
+                break;
+        }
+    }
+    return s;
+    }
+};
