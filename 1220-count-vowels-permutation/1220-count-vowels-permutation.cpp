6f8123d5dd3e0ba49deea867e67c98fd6a3c55e0
RITIK ARORA <ritikarora995@gmail.com>
2022-08-08 00:33:51 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.33%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-  int x=1e9+7;
-    int rec(char ch, int n,long int i,vector<vector<long int>>&dp){
-        if(i==n) return 1;
-        if(dp[ch-'a'][i]!=-1) return dp[ch-'a'][i]%x;
-        if(ch=='a')
-        {   dp[ch-'a'][i]=rec('e',n,i+1,dp);
-            return dp[ch-'a'][i];
+    int countVowelPermutation(int n) {
+        long  a=1,e=1,i=1,o=1,u=1;
+        long  a2,e2,i2,o2,u2;
+        int mod=1e9+7;
+        for(int j=2;j<=n;j++){
+            a2=((e+i)%mod+u)%mod;
+            e2=(a+i)%mod;
+            i2=(e+o)%mod;
+            o2=(i)%mod;
+            u2=(o+i)%mod;
+            a=a2,e=e2,i=i2,o=o2,u=u2;
         }
-        else if(ch=='e')
-        {
-            dp[ch-'a'][i]= (rec('a',n,i+1,dp)+rec('i',n,i+1,dp))%x;
-             return dp[ch-'a'][i];
-        }
-        else if (ch=='i'){
-            dp[ch-'a'][i]=(((rec('a',n,i+1,dp)+rec('e',n,i+1,dp))%x+rec('o',n,i+1,dp))%x+rec('u',n,i+1,dp))%x;
-            return dp[ch-'a'][i]%x;
-        }
-        else if(ch=='o'){
-            dp[ch-'a'][i]=( rec('i',n,i+1,dp)+rec('u',n,i+1,dp))%x;
-            return dp[ch-'a'][i]%x;
-        } 
-        else if(ch=='u') {
-            dp[ch-'a'][i]=(rec('a',n,i+1,dp))%x;
-            return dp[ch-'a'][i]%x;
-        }
-        return ((((rec('a',n,i+1,dp)+rec('e',n,i+1,dp))%x+rec('i',n,i+1,dp))%x+rec('o',n,i+1,dp))%x+rec('u',n,i+1,dp))%x;
-    }
-    int countVowelPermutation(int n)
-    {
-         char ch='z';
-         vector<vector<long int>>dp(26,vector<long int>(n+1,-1));
-         return rec(ch,n,0,dp);
+        return (a+e+i+o+u)%mod;
     }
 };
