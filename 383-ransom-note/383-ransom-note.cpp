20ab119a59599f06c7888c6b3f7df0712b518a45
RITIK ARORA <ritikarora995@gmail.com>
2022-08-25 16:54:14 +0530

    Time: 31 ms (41.50%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) 
     {
-        unordered_map<char,int>umap;
-        for(int i=0;i<magazine.size();i++){
-            umap[magazine[i]]++;
-        }
-        bool flag;
-        for(int i=0;i<ransomNote.size();i++)
-        {
-            if(umap[ransomNote[i]]>0){
-                flag=1;
-                umap[ransomNote[i]]--;
-            }
-            else{
-                return 0;
-            }
-        }
-        return flag;
+               int count[26] = {0};
+        for(char ch : magazine)
+            count[ch - 'a']++;
+        
+        for(char ch : ransomNote)
+            if(count[ch - 'a']-- <= 0)
+                return false;
+        
+        return true;
+    
+
     }
 };
