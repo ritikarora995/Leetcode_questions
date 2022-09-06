6cca37a41df4cef033e9332ffa2e03535cd0eac0
RITIK ARORA <ritikarora995@gmail.com>
2022-09-07 01:30:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (67.95%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int numTrees1(int n,vector<int>&dp) {
-    if(n<=1) return 1;
-    int ans=0;
-        if(dp[n-1]!=-1) return dp[n];
-     for(int i=1;i<=n;i++)
-     {
-        ans+= numTrees1(i-1,dp)*numTrees1(n-i,dp);
-     } 
-     dp[n]=ans;
-     return dp[n];
-    }
-    int numTrees(int n){
-        vector<int>dp(n+1,-1);
-        return numTrees1(n,dp);
+    int numTrees(int n) {
+      vector<int>dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                  dp[i]+=dp[i-j]*dp[j-1];
+            }
+        }
+        return dp[n];
     }
 };
