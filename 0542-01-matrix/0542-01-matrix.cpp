4c6f9f73b79d4923f258e461cf60f800e506b872
RITIK ARORA <ritikarora995@gmail.com>
2022-10-25 15:41:19 +0530

    Time: 176 ms (43.97%), Space: 30.4 MB (46.11%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isvalid(int i,int j,int n,int m){
+        if(i<0 || j<0 || i>=n || j>=m) return 0;
+        return 1;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){  
+             if(mat[i][j]==0) {
+                 q.push({i,j});
+                 ans[i][j]=0;
+             }
+        }}
+            while(!q.empty()){
+                int curr_x=q.front().first;
+                int curr_y=q.front().second;
+                if(isvalid(curr_x-1,curr_y,n,m)==1 && ans[curr_x-1][curr_y]==-1 )                 {
+                    q.push({curr_x-1,curr_y});
+                    ans[curr_x-1][curr_y]=ans[curr_x][curr_y]+1;
+                }
+                 if(isvalid(curr_x+1,curr_y,n,m)==1 && ans[curr_x+1][curr_y]==-1 )                 {
+                    q.push({curr_x+1,curr_y});
+                    ans[curr_x+1][curr_y]=ans[curr_x][curr_y]+1;
+                }
+                 if(isvalid(curr_x,curr_y+1,n,m)==1 && ans[curr_x][curr_y+1]==-1 )                 {
+                    q.push({curr_x,curr_y+1});
+                    ans[curr_x][curr_y+1]=ans[curr_x][curr_y]+1;
+                }
+                 if(isvalid(curr_x,curr_y-1,n,m)==1 && ans[curr_x][curr_y-1]==-1 )                 {
+                    q.push({curr_x,curr_y-1});
+                    ans[curr_x][curr_y-1]=ans[curr_x][curr_y]+1;
+                }
+                q.pop();
+            }
+            return ans;
+    }
+};
