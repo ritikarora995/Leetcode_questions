442434bb4d4481ce969270f55c05509f24ea7d9b
RITIK ARORA <ritikarora995@gmail.com>
2022-10-28 17:23:59 +0530

    Time: 190 ms (32.07%), Space: 48.3 MB (78.09%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& A) {
+int n=A.size();
+    int last=n-1,i,j;
+    for(i=n-2;i>=0;i--){
+        if(i+A[i]>=last)last=i;
+    }
+        cout<<last<<endl;
+    return last<=0;
+}
+
+};
